0cd2b22b20785eca4b063985290241b42994087c
clicknetcafe <rama4shared@gmail.com>
2022-09-03 13:21:33 +0800

    opsi stiker 60fps

--- plugins/creator-sticker.js
+++ plugins/creator-sticker.js
@@ -1,28 +1,30 @@
 import { createSticker, StickerTypes } from 'wa-sticker-formatter'
-import { addExif, videoToWebp } from '../lib/sticker.js'
+import { addExif, videoToWebp, videoToWebp2 } from '../lib/sticker.js'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 	try {
 		let q = m.quoted ? m.quoted : m
 		let mime = (q.msg || q).mimetype || q.mediaType || ''
-		let img = await q.download?.()
+	    let waluh = /60|halus|smoo?th/.test(command.toLowerCase())
 		if (/webp|image/g.test(mime) || q.gifPlayback == true) {
+			let img = await q.download?.()
 			let buffer = await createSticker(img, { pack: packname, author: author, type: StickerTypes.FULL })
 			await conn.sendFile(m.chat, buffer, 'sticker.webp', '', m)
 		} else if (/video/g.test(mime)) {
 			if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+			let img = await q.download?.()
 			try {
 				if (q.message.videoMessage.gifPlayback == true) {
 					let buffer = await createSticker(img, { pack: packname, author: author, type: StickerTypes.FULL })
 					await conn.sendFile(m.chat, buffer, 'sticker.webp', '', m)
 				} else {
-					let buff = await videoToWebp(img)
+					let buff = waluh ? await videoToWebp2(img) : await videoToWebp(img)
 					let buffer = await addExif(buff, packname, author)
 					await conn.sendFile(m.chat, buffer, 'sticker.webp', '', m)
 				}
 			} catch (e) {
 				console.log(e)
-				let buff = await videoToWebp(img)
+				let buff = waluh ? await videoToWebp2(img) : await videoToWebp(img)
 				let buffer = await addExif(buff, packname, author)
 				await conn.sendFile(m.chat, buffer, 'sticker.webp', '', m)
 			}
@@ -37,6 +39,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 handler.help = ['stiker (caption|reply media)', 'stiker <url>']
 handler.tags = ['creator']
-handler.command = /^(s(tic?ker)?(gif)?)$/i
+handler.command = /^(s(tic?ker)?(gif)?(60|halus|smoo?th)?(fps)?)$/i
 
 export default handler
