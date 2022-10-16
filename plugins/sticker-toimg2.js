687bb42079b11bfc9e9137c7dc02997f04e857e7
clicknetcafe <rama4shared@gmail.com>
2022-10-16 08:38:22 +0800

    undo

--- plugins/sticker-toimg2.js
+++ plugins/sticker-toimg2.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     if (!/sticker/.test(mime)) throw notStickerMessage
     let media = await q.download()
     let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendMessage(m.chat, { image: out, caption: `*DONE*` }, { quoted: m })
+    await conn.sendFile(m.chat, out, 'out.png', '*DONE*', m)
 }
 
 handler.help = ['toimg2']
--- plugins/tool-tomp4.js
+++ plugins/tool-tomp4.js
@@ -1,14 +1,14 @@
 import { webp2mp4 } from '../lib/webp2mp4.js'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-    const notStickerMessage = `Reply sticker with command *${usedPrefix + command}*`
-    if (!m.quoted) throw notStickerMessage
-    const q = m.quoted || m
-    let mime = q.mediaType || ''
-    if (!/sticker/.test(mime)) throw notStickerMessage
-    let media = await q.download()
-    let out = await webp2mp4(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendMessage(m.chat, { video: out, caption: `* DONE*` }, { quoted: m })
+	const notStickerMessage = `Reply sticker with command *${usedPrefix + command}*`
+	if (!m.quoted) throw notStickerMessage
+	const q = m.quoted || m
+	let mime = q.mediaType || ''
+	if (!/sticker/.test(mime)) throw notStickerMessage
+	let media = await q.download()
+	let out = await webp2mp4(media).catch(_ => null) || Buffer.alloc(0)
+	await conn.sendFile(m.chat, out, 'out.mp4', '*DONE*', m)
 }
 
 handler.help = ['tomp4']
