0ffa8db2ea5a02ec2654189c2830f558f7927329
clicknetcafe <rama4shared@gmail.com>
2022-11-04 23:21:56 +0800

    wikimedia

---
+++ plugins/search-wikimedia.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+    if (!text) return m.reply(`Example: *${usedPrefix + command} indonesia*`)
+	try {
+		let res = await fetch(`https://botcahx.ddns.net/api/search/wikimedia?text=${encodeURIComponent(text)}`)
+		let anu = await res.json()
+		anu = anu.result[Math.floor(Math.random()*anu.result.length)]
+		await conn.sendButton(m.chat, `${command} > ${text}\n\n${anu.title}\n_${anu.source}_`, packname + ' - ' + author, anu.image, [[`⧑ next ${command} ⧑`, `${usedPrefix + command} ${text}`]], m)
+	} catch (e) {
+		console.log(e)
+		m.reply(`No Result Found.`)
+	}
+}
+
+handler.help = ['wikimedia <text>']
+handler.tags = ['searching']
+handler.command = /^(wikimedia)$/i
+
+export default handler
