3b702249079b4f9459d9ce20f3ce3157e369d8c9
clicknetcafe <rama4shared@gmail.com>
2022-12-02 23:26:40 +0800

    dipikir pikir kepikiran

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,19 +1,22 @@
 import db from '../lib/database.js'
 
 const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+const linkLain = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/g
 
-export async function before(m, { conn, isAdmin, isBotAdmin }) {
+export async function before(m, { conn, isAdmin, text, isBotAdmin }) {
 	if (m.isBaileys && m.fromMe) return !0
 	if (!m.isGroup) return !1
 	let chat = db.data.chats[m.chat]
 	const isGroupLink = linkRegex.exec(m.text)
 	if (chat.antiLink && isGroupLink && !isAdmin) {
 		if (isBotAdmin) {
-			const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-			if (m.text.includes(linkThisGroup)) return !0
+			const p = await this.groupInviteCode(m.chat)
+			const linkThisGroup = `https://chat.whatsapp.com/${p}`
+			text = [...m.text.matchAll(linkLain)].map(v => v[1]).filter(v => !v.includes(p))
+			if (m.text.includes(linkThisGroup) && text.length == 0) return !0
 		}
-		if (!m.key.fromMe && isBotAdmin) await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.id, participant: m.sender } })
-		await conn.sendButton(m.chat, `*Group link detect!*${isBotAdmin ? '' : '\n\n_Bot not admin_  t_t'}`, packname + " - " + author, ['off antilink', '/disable antilink'], m)
+		if (!m.fromMe && isBotAdmin) await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.id, participant: m.sender } })
+		await conn.sendMessage(m.chat, { text: `@${(m.sender || '').replace(/@s\.whatsapp\.net/g, '')} *terdeteksi* mengirim Link Group!`, mentions: [m.sender] }, { quoted: fkontak })
 	}
 	return !0
 }
