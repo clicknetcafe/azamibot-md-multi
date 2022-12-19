9a600db10a388035d14d63cf91256e4ba4d23074
clicknetcafe <rama4shared@gmail.com>
2022-12-20 01:48:07 +0800

    setteks2

--- plugins/owner-setteks2.js
+++ plugins/owner-setteks2.js
@@ -2,7 +2,7 @@ import db from '../lib/database.js'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 	if (!text) throw `[ ! ] Input teks.`
-	if (text = 'default') text = ''
+	if (text == 'default') text = ''
 	db.data.datas.teksjadibot = text
 	await m.reply(`Berhasil *${command} :*\n\n${text}`)
 }
