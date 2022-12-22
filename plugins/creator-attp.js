d3d7e5e43fd717443e87271359955c8c945ee11b
clicknetcafe <rama4shared@gmail.com>
2022-12-23 01:14:05 +0800

    swa-formatter for better speed

--- plugins/creator-attp.js
+++ plugins/creator-attp.js
@@ -1,28 +1,29 @@
-import { sticker } from '../lib/sticker.js'
+import { createSticker } from 'wa-sticker-formatter'
 import fs from 'fs'
 
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-    text = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.quoted && m.quoted.caption ? m.quoted.caption : m.quoted && m.quoted.description ? m.quoted.description : ''
-    if (!text) throw `Example : ${usedPrefix + command} Lagi Ruwet`
-    const res = `https://api.lolhuman.xyz/api/${command}?apikey=${global.api}&text=${encodeURIComponent(text.substring(0, 151))}`
-    const res2 = `https://xteam.xyz/attp?file&text=${encodeURIComponent(text.substring(0, 151))}`
-    let ztick = fs.readFileSync(`./media/sticker/bronya.webp`)
-    try {
-        if (command == 'attp' || command == 'attp2') {
-            let stiker = await sticker(false, res, global.packname, global.author)
-            await conn.sendFile(m.chat, stiker, 'atet.webp', '', m)
-        } else if (command == 'attp3') {
-            let stiker = await sticker(false, res2, global.packname, global.author)
-            await conn.sendFile(m.chat, stiker, 'atet.webp', '', m)
-        } else {
-            const stiker = await sticker(false, res, global.packname, global.author)
-            await conn.sendFile(m.chat, stiker, 'atet.webp', '', m)
-        }
-    } catch (e) {
-        console.log(e)
-        await conn.sendFile(m.chat, ztick, 'atet.webp', '', m)
-    }
+	text = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.quoted && m.quoted.caption ? m.quoted.caption : m.quoted && m.quoted.description ? m.quoted.description : ''
+	if (!text) throw `Example : ${usedPrefix + command} Lagi Ruwet`
+	const res = `https://api.lolhuman.xyz/api/${command}?apikey=${global.api}&text=${encodeURIComponent(text.substring(0, 151))}`
+	const res2 = `https://xteam.xyz/attp?file&text=${encodeURIComponent(text.substring(0, 151))}`
+	let ztick = fs.readFileSync(`./media/sticker/bronya.webp`)
+	try {
+		if (command == 'attp' || command == 'attp2') {
+			let stiker = await createSticker(res, { pack: packname, author: author })
+			await conn.sendFile(m.chat, stiker, 'atet.webp', '', m)
+		} else if (command == 'attp3') {
+			let stiker = await createSticker(res2, { pack: packname, author: author })
+			await conn.sendFile(m.chat, stiker, 'atet.webp', '', m)
+		} else {
+			let stiker = await createSticker(res, { pack: packname, author: author })
+			await conn.sendFile(m.chat, stiker, 'atet.webp', '', m)
+		}
+	} catch (e) {
+		console.log(e)
+		await conn.sendFile(m.chat, ztick, 'atet.webp', '', m)
+	}
 }
+
 handler.help = ['ttp','ttp2 -> ttp6','attp','attp2','attp3']
 handler.tags = ['creator']
 handler.command = /^((ttp(2|3|4|5|6)?)|(attp(2|3)?))$/i
---
+++ plugins/creator-openaidall-e.js
@@ -0,0 +1,23 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+	if (!text) throw `[!] Masukkan detail teks.`
+	try {
+		let res = await fetch(`https://api.lolhuman.xyz/api/dall-e?apikey=${global.api}&text=${encodeURIComponent(text)}`)
+		let anu = Buffer.from(await res.arrayBuffer())
+		if (Buffer.byteLength(anu) < 22000) throw Error(`[!] Error : Buffer not found.`)
+		await conn.sendMessage(m.chat, { image: anu, caption: `Open AI Dall E :\n${text}` }, { quoted: m })
+	} catch (e) {
+		console.log(e)
+		throw `Fitur Error.`
+	}
+}
+
+handler.help = ['aiimage']
+handler.tags = ['creator']
+handler.command = /^((open)?ai(image|gambar|foto|dalle))$/i
+
+handler.premium = true
+handler.limit = true
+
+export default handler
