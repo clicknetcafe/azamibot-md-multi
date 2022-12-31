2774a966605f858f354f0a10e2b90c861585b3ce
clicknetcafe <rama4shared@gmail.com>
2022-12-31 15:07:55 +0800

    akinator fix

--- plugins/game-akinator_ans.js
+++ plugins/game-akinator_ans.js
@@ -9,7 +9,7 @@ export async function before(m) {
 	if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text) return !0
 	let aki = db.data.users[m.sender].akinator
 	if (!aki.sesi || m.quoted.id != aki.soal.key.id) return
-	if (!somematch(['0','1','2','3','4','5'], m.text)) return conn.sendMessage(m.chat, { text: `[!] Jawab dengan angka 1, 2, 3, 4, atau 5\n\n${teks}` }, { quoted: aki.soal })
+	if (!somematch(['0','1','2','3','4','5'], m.text)) return this.sendMessage(m.chat, { text: `[!] Jawab dengan angka 1, 2, 3, 4, atau 5\n\n${teks}` }, { quoted: aki.soal })
 	let { server, frontaddr, session, signature, question, progression, step } = aki
 	if (step == '0' && m.text == '5') return m.reply('Anda telah mencapai pertanyaan pertama')
 	let res, anu, soal
@@ -24,11 +24,11 @@ export async function before(m) {
 		}
 		anu = anu.result
 		if (anu.name) {
-			await conn.sendMessage(m.chat, { image: { url: anu.image }, caption: `🎮 *Akinator Answer* 🎮\n\nDia adalah *${anu.name}*\n_${anu.description}_`, mentions: [m.sender] }, { quoted: m })
+			await this.sendMessage(m.chat, { image: { url: anu.image }, caption: `🎮 *Akinator Answer* 🎮\n\nDia adalah *${anu.name}*\n_${anu.description}_`, mentions: [m.sender] }, { quoted: m })
 			aki.sesi = false
 			aki.soal = null
 		} else {
-			soal = await conn.sendMessage(m.chat, { text: `🎮 *Akinator* 🎮\n_step ${anu.step} ( ${anu.progression.toFixed(2)} % )_\n\n@${m.sender.split('@')[0]}\n    ${anu.question}\n\n${teks}`, mentions: [m.sender] }, { quoted: m })
+			soal = await this.sendMessage(m.chat, { text: `🎮 *Akinator* 🎮\n_step ${anu.step} ( ${anu.progression.toFixed(2)} % )_\n\n@${m.sender.split('@')[0]}\n    ${anu.question}\n\n${teks}`, mentions: [m.sender] }, { quoted: m })
 			aki.soal = soal
 			aki.step = anu.step
 			aki.progression = anu.progression
