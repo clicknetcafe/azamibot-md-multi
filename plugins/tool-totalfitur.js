a40467f2ad5e09858a6d4df9bd17bd6f57adf177
clicknetcafe <rama4shared@gmail.com>
2023-01-09 03:43:57 +0800

    simple totalfitur

--- plugins/tool-totalfitur.js
+++ plugins/tool-totalfitur.js
@@ -1,34 +1,17 @@
 import { plugins } from '../lib/plugins.js'
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-	let menu = Object.values(plugins).filter(v => v.help && !v.disabled).map(v => v.help).flat(1)
-	let menu2 = Object.values(plugins).filter(v => v.menuanime && !v.disabled).map(v => v.menuanime).flat(1)
-	let menu3 = Object.values(plugins).filter(v => v.menudownload && !v.disabled).map(v => v.menudownload).flat(1)
-	let menu4 = Object.values(plugins).filter(v => v.menuephoto && !v.disabled).map(v => v.menuephoto).flat(1)
-	let menu5 = Object.values(plugins).filter(v => v.menueditor && !v.disabled).map(v => v.menueditor).flat(1)
-	let menu6 = Object.values(plugins).filter(v => v.menufun && !v.disabled).map(v => v.menufun).flat(1)
-	let menu7 = Object.values(plugins).filter(v => v.menugenshin && !v.disabled).map(v => v.menugenshin).flat(1)
-	let menu8 = Object.values(plugins).filter(v => v.menugroup && !v.disabled).map(v => v.menugroup).flat(1)
-	let menu9 = Object.values(plugins).filter(v => v.menunsfw && !v.disabled).map(v => v.menunsfw).flat(1)
-	let menu10 = Object.values(plugins).filter(v => v.menuowner && !v.disabled).map(v => v.menuowner).flat(1)
-	let menu11 = Object.values(plugins).filter(v => v.menuoxy && !v.disabled).map(v => v.menuoxy).flat(1)
-	let menu12 = Object.values(plugins).filter(v => v.menutextpro && !v.disabled).map(v => v.menutextpro).flat(1)
+const all = ['help', ...['anime','download','ephoto','editor','fun','genshin','group','nsfw','owner','oxy','store','textpro'].map(v => 'menu' + v)]
 
-	let txt = `*Total Fitur : ${menu.length + menu2.length + menu3.length + menu4.length + menu5.length + menu6.length + menu7.length + menu8.length + menu9.length + menu10.length + menu11.length + menu12.length}* Commands\n\n`
-	txt += `*[ FRACTION LIST ]*\n\n`
-	txt += `*⭔ Main Menu :* ${menu.length} fitur\n`
-	txt += `*⭔ Anime :* ${menu2.length} fitur\n`
-	txt += `*⭔ Download :* ${menu3.length} fitur\n`
-	txt += `*⭔ Ephoto 360 :* ${menu4.length} fitur\n`
-	txt += `*⭔ Editor :* ${menu5.length} fitur\n`
-	txt += `*⭔ Fun & Games :* ${menu6.length} fitur\n`
-	txt += `*⭔ Genshin :* ${menu7.length} fitur\n`
-	txt += `*⭔ Group :* ${menu8.length} fitur\n`
-	txt += `*⭔ NSFW :* ${menu9.length} fitur\n`
-	txt += `*⭔ Owner :* ${menu10.length} fitur\n`
-	txt += `*⭔ Photo Oxy :* ${menu11.length} fitur\n`
-	txt += `*⭔ Text Pro Me :* ${menu12.length} fitur`
-	await m.reply(txt)
+let handler = async (m) => {
+	let count = 0
+	let txt = `*[ FRACTION LIST ]*\n`
+	for (let x of all) {
+		let cmd = Object.values(plugins).filter(v => v[x] && !v.disabled).map(v => v[x]).flat().length
+		count += cmd
+		txt += `\n*⭔ ${x == 'help' ? 'Main Menu' : x} :* ${cmd} fitur`
+	}
+	txt += `\n\n*Total Fitur : ${count} Commands*`
+	await m.reply(txt.replace(/menu/g, ''))
 }
 
 handler.help = ['totalfitur']
