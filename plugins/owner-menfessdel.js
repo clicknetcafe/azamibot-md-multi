061689340848a7876daaf15c199a65f1be87f5ca
clicknetcafe <rama4shared@gmail.com>
2023-01-17 22:49:39 +0800

    replace

--- plugins/owner-menfessdel.js
+++ plugins/owner-menfessdel.js
@@ -1,15 +1,13 @@
 import db from '../lib/database.js'
 
 let handler = async(m) => {
-	let a = await db.data.datas.menfess
-	if (a.length > 2000) {
-		let half = await Math.floor(a.length / 2)
-		let array1 = await a.slice(half, a.length)
-		db.data.datas.menfess = await array1
-		await m.reply(`[ Sukses ]\n\nData menfess sekarang : *${array1.length}* ${array1.length > 2000 ? '( dapat dikurangi lagi )' : ''}`)
-	} else {
-		m.reply(`[ ! ] Data sampah menfess harus *di atas 2000*.\n\nData menfes sekarang : *${a.length}*`)
-	}
+	let menfess = db.data.datas.menfess
+	let a = Object.keys(menfess)
+	if (a.length < 2000) throw `[ ! ] Data sampah menfess harus *di atas 2000*.\n\nData menfes sekarang : *${a.length}*`
+	let half = await Math.floor(a.length / 2)
+	a = a.slice(0, half)
+	for (let x of a) { delete menfess[x] }
+	await m.reply(`[ Sukses ]\n\nData menfess sekarang : *${a.length}* ${a.length > 2000 ? '( dapat dikurangi lagi )' : ''}`)
 }
 
 handler.menuowner = ['menfessdel']
