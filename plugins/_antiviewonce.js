a4621bb9ba8ea8e0a5a05654b2c98cf97c9efac1
clicknetcafe <rama4shared@gmail.com>
2023-01-28 11:59:15 +0800

    better handling viewOnce message

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -1,9 +1,20 @@
 import db from '../lib/database.js'
-import util, { format } from 'util'
+import { downloadContentFromMessage } from '@adiwajshing/baileys'
 
 export async function before(m) {
 	if (!m.isGroup) return !1
-	if (!m.message) return !0
-	if (db.data.chats[m.chat].viewonce && m.message.viewOnceMessageV2) await this.sendMessage(m.chat, { text: format(util.inspect(m.message.viewOnceMessageV2, { depth: null })) }, { quoted: fkontak })
+	if (m.mtype != 'viewOnceMessageV2') return !0
+	if (db.data.chats[m.chat].viewonce) {
+		let msg = m.message.viewOnceMessageV2.message
+		let type = Object.keys(msg)[0]
+		let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
+		let buffer = Buffer.from([])
+		for await (const chunk of media) {
+			buffer = Buffer.concat([buffer, chunk])
+		}
+		let i = `[ ANTIVIEWONCE AKTIF ]\n\n👾 *Sender* : @${m.sender.split`@`[0]}${msg[type].caption ? `\n\n*Caption :*\n${msg[type].caption}` : ''}`
+		let j = msg[type].caption ? [m.sender, ...[...msg[type].caption.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')] : [m.sender]
+		await this.sendFile(m.chat, buffer, '', i, null, false, { mentions: j, quoted: fkontak })
+	}
 	return !0
 }
--- plugins/tool-readviewonce.js
+++ plugins/tool-readviewonce.js
@@ -1,13 +1,21 @@
+import { downloadContentFromMessage } from '@adiwajshing/baileys'
+
 let handler = async (m, { conn }) => {
-	if (!m.quoted) throw 'where\'s message?'
-	if (m.quoted.mtype !== 'viewOnceMessage') throw 'Itu bukan pesan viewOnce'
-	const buffer = await m.quoted.download()
-	const media = m.quoted.mediaMessage[m.quoted.mediaType]
-	conn.sendFile(m.chat, buffer, /video/.test(media.mimetype) ? 'video.mp4' : 'image.jpg', media.caption || '', m)
+	if (!m.quoted) throw 'Quote pesan viewOnce'
+	if (m.quoted.mtype != 'viewOnceMessageV2') throw 'Itu bukan pesan viewOnce'
+	let msg = m.quoted.message
+	let type = Object.keys(msg)[0]
+	let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
+	let buffer = Buffer.from([])
+	for await (const chunk of media) {
+		buffer = Buffer.concat([buffer, chunk])
+	}
+	let j = msg[type].caption ? [...msg[type].caption.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')  : []
+	await conn.sendFile(m.chat, buffer, '', msg[type].caption || '', null, false, { mentions: j, quoted: m })
 }
 
 handler.help = ['readviewonce']
 handler.tags = ['tools']
-handler.command = /^((read)?viewonce)$/i
+handler.command = /^((read)?viewonce|rvo)$/i
 
 export default handler
