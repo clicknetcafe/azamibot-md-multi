0ae657777f29e75ea657737d44808d5cfcb26772
clicknetcafe <rama4shared@gmail.com>
2023-02-07 03:21:59 +0800

    return error when !type or type = 'xml'
    you still can process xml file with conn.sendMessage or conn.getFile

--- plugins/download-tiktokslide.js
+++ plugins/download-tiktokslide.js
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch'
+import { delay,ranNumb } from '../lib/others.js'
 
-let handler = async(m, { conn, text, usedPrefix, command }) => {
+let handler = async(m, { conn, text, usedPrefix, command, isPrems }) => {
 	if (!text) throw `Example: ${usedPrefix + command} https://vt.tiktok.com/ZS81qJD5v/`
 	if (!(text.includes('http://') || text.includes('https://'))) return m.reply(`url invalid, please input a valid url. Try with add http:// or https://`)
 	if (!text.includes('tiktok.com')) return m.reply(`Invalid Tiktok URL.`)
@@ -10,11 +11,16 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 		if (anu.status != '200') throw Error(anu.message)
 		anu = anu.result
 		if (anu.length == 0) throw Error('Error : no data')
-		let c = 0
+		let c = 0, d = anu.length
+		if (!isPrems && anu.length > 7) {
+			anu = anu.slice(0, 7)
+			await conn.reply(m.sender, `_[!] (bukan user premium)_ limit maksimal 6 Slide.`, m)
+		}
 		for (let x of anu) {
-			if (c == 0) await conn.sendFile(m.chat, x, '', `Mengirim 1 dari ${anu.length} slide gambar.\n_(Sisanya akan dikirim via chat pribadi.)_`, m)
+			if (c == 0) await conn.sendFile(m.chat, x, '', `Mengirim 1 dari ${d} slide gambar.\n_(Sisanya akan dikirim via chat pribadi.)_`, m)
 			else await conn.sendFile(m.sender, x, '', '', m)
 			c += 1
+			await delay(isPrems ? ranNumb(700, 1000) : ranNumb(800, 1500))
 		}
 	} catch (e) {
 		console.log(e)
