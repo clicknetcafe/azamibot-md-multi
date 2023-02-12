0dd884aa1d704636d31b84e16504cd9c7c9b0351
clicknetcafe <rama4shared@gmail.com>
2023-02-12 10:01:20 +0800

    fix toaudio

--- plugins/tool-tomp3.js
+++ plugins/tool-tomp3.js
@@ -1,21 +1,14 @@
-import { ranNumb, padLead } from '../lib/others.js'
 import { toAudio } from '../lib/converter.js'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-		let q = m.quoted ? m.quoted : m
-		let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-		if (!/video|audio/.test(mime)) throw `Reply video/audio with caption *${usedPrefix + command}*`
-		let media = await q.download?.()
-		if (!media) throw 'Can\'t download media'
-		let audio = await toAudio(media, 'mp4')
-		if (!audio.data) throw 'Can\'t convert media to audio'
-		if (command.includes('toa')) {
-			await conn.sendMsg(m.chat, { audio: audio.data, mimetype: 'audio/mp4' }, { quoted: m })
-		} else {
-			let date = new Date().toISOString().replace('-', '').split('T')[0].replace('-', '')
-			let meh = padLead(ranNumb(9999), 4)
-			await conn.sendMsg(m.chat, {document: audio.data, mimetype: 'audio/mpeg', fileName: `AUD-${date}-WA${meh}.mp3`}, { quoted : m })
-		}
+	let q = m.quoted ? m.quoted : m
+	let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
+	if (!/video/.test(mime)) throw `Reply video/audio with caption *${usedPrefix + command}*`
+	let media = await q.download?.()
+	let audio = await toAudio(media, 'mp4')
+	audio = await audio.toBuffer()
+	if (/toa/.test(command)) await conn.sendMsg(m.chat, { audio: audio, mimetype: 'audio/mp4' }, { quoted: m })
+	else await conn.sendMsg(m.chat, {document: audio, mimetype: 'audio/mpeg', fileName: 'Convert by AzamiBot.mp3' }, { quoted : m })
 }
 
 handler.help = ['tomp3']
