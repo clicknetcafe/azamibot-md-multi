8f5939ece9d0a4db68ce61275d5ccfd7d0adf112
clicknetcafe <rama4shared@gmail.com>
2023-02-17 01:15:27 +0800

    update package

--- plugins/tool-tomp3.js
+++ plugins/tool-tomp3.js
@@ -3,10 +3,11 @@ import { toAudio } from '../lib/converter.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
 	let q = m.quoted ? m.quoted : m
 	let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-	if (!/audio|video/.test(mime)) throw `Reply video/audio with caption *${usedPrefix + command}*`
-	let media = await q.download?.()
-	if (/video/.test(mime)) media = await toAudio(media, 'mp4').then((data) => data.toBuffer())
-	await conn.sendMsg(m.chat, /toa/.test(command) ? { audio: media, mimetype: 'audio/mp4' } : { document: media, mimetype: 'audio/mpeg', fileName: 'Convert by AzamiBot.mp3' }, { quoted: m })
+	if (/audio|video/.test(mime)) {
+		let media = await q.download?.()
+		if (/video/.test(mime)) media = await toAudio(media, 'mp4').then((data) => data.toBuffer())
+		await conn.sendMsg(m.chat, /toa/.test(command) ? { audio: media, mimetype: 'audio/mp4' } : { document: media, mimetype: 'audio/mpeg', fileName: 'Convert by AzamiBot.mp3' }, { quoted: m })
+	} else throw `Reply video/audio with caption *${usedPrefix + command}*`
 }
 
 handler.help = ['tomp3']
--- plugins/tool-tovn.js
+++ plugins/tool-tovn.js
@@ -3,10 +3,11 @@ import { toPTT } from '../lib/converter.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
 	let q = m.quoted ? m.quoted : m
 	let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-	if (!/audio|video/.test(mime)) throw `Reply video/audio with caption *${usedPrefix + command}*`
-	let media = await q.download?.()
-	if (/video/.test(mime)) media = await toPTT(media, 'mp4').then((data) => data.toBuffer())
-	await conn.sendMsg(m.chat, { audio: media, mimetype: 'audio/mp4', ptt: true }, { quoted: m })
+	if (/audio|video/.test(mime)) {
+		let media = await q.download?.()
+		if (/video/.test(mime)) media = await toPTT(media, 'mp4').then((data) => data.toBuffer())
+		await conn.sendMsg(m.chat, { audio: media, mimetype: 'audio/mp4', ptt: true }, { quoted: m })
+	} else throw `Reply video/audio with caption *${usedPrefix + command}*`
 }
 
 handler.help = ['tovn (reply)']
