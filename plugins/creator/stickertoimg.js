4746f30b7a71533704655686f4a3474c41c79931
clicknetcafe <rama4shared@gmail.com>
2023-09-27 18:57:42 +0800

    mime png

--- plugins/creator/stickertoimg.js
+++ plugins/creator/stickertoimg.js
@@ -1,14 +1,27 @@
+import { spawn } from 'child_process'
+import { format } from 'util'
+
 let handler = async (m, { conn, command, usedPrefix }) => {
 	let q = m.quoted
-	try {
-		if (q && /sticker/.test(q.mtype)) {
-			if (q.isAnimated) return m.reply(`Gunakan *${usedPrefix}tomp4* untuk stiker bergerak`)
-			let img = await m.quoted.download()
-			await conn.sendMsg(m.chat, { image: img, jpegThumbnail: img, caption: `*DONE*` }, { quoted: m })
-		} else return m.reply('Reply / tag Sticker')
-	} catch (e) {
-		console.log(e)
-	}
+	if (q && /sticker/.test(q.mtype)) {
+		if (q.isAnimated) return m.reply(`Gunakan *${usedPrefix}tomp4* untuk stiker bergerak`)
+		let img = await m.quoted.download()
+		try {
+			let bufs = []
+			const [_spawnprocess, ..._spawnargs] = [...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []), 'convert', 'webp:-', 'png:-']
+			let im = spawn(_spawnprocess, _spawnargs)
+			im.on('error', e => m.reply(format(e)))
+			im.stdout.on('data', chunk => bufs.push(chunk))
+			im.stdin.write(img)
+			im.stdin.end()
+			im.on('exit', async () => {
+				await conn.sendFile(m.chat, Buffer.concat(bufs), 'image.png', '*DONE*', m)
+			})
+		} catch (e) {
+			console.log(e)
+			await conn.sendMsg(m.chat, { image: img, jpegThumbnail: img, caption: '*DONE*' }, { quoted: m })
+		}
+	} else return m.reply('Reply / tag Sticker')
 }
 
 handler.help = ['toimg']
