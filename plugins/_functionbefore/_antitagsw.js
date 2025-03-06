67d4300737698a7b23ee958a3f4a7c6ab478c163
zerenity <rama4shared@gmail.com>
2025-03-06 13:00:17 +0800

    animated sticker

--- plugins/_functionbefore/_antitagsw.js
+++ plugins/_functionbefore/_antitagsw.js
@@ -5,14 +5,16 @@ export async function before(m, { isAdmin, text, isBotAdmin }) {
 	if (m.isGroup || m.chat == 'status@broadcast') {
 		this.cektag = this.cektag ? this.cektag : {}
 		if (m.isGroup) {
-			if (!m.message || !db.data.chats[m.chat]?.antitagsw) return !1
-			if (Object.keys(m.message).length === 0) {
-				if (this.cektag.user != m.key.participant) return !1
-				const { time } = this.cektag
-				if ([time, time+1, time+2].includes(m.messageTimestamp?.low)) {
-					if (isBotAdmin)
-						await this.sendMsg(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.id, participant: m.sender } })
-				} else this.cektag = {}
+			if (!db.data.chats[m.chat]?.antitagsw) return !1
+			if (Object.keys(m.message || {}).length === 0) {
+				if (this.cektag.user == m.key.participant) {
+					let { time } = this.cektag
+					if (m.messageTimestamp?.low >= time && m.messageTimestamp?.low <= time + 3) {
+						if (isBotAdmin)
+							await this
+						.sendMsg(m.chat, { delete: { remoteJid: m.key.remoteJid, fromMe: false, id: m.key.id, participant: m.sender } })
+					} else delete this.cektag.user
+				}
 			}
 		} else this.cektag = { user: m.key.participant, time: m.messageTimestamp?.low }
 	}
--- plugins/creator/sticker.js
+++ plugins/creator/sticker.js
@@ -13,17 +13,19 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 			let fps = command.replace(/\D/g, '')
 			if (/image|video/g.test(mime)) {
 				let ch, img = await q.download?.()
+				let sec = (q.msg || q)?.seconds
 				if (/video/g.test(mime)) {
-					if ((q.msg || q).seconds > 30) return m.reply('Maksimal 10 detik!')
+					if (sec > 11) return m.reply('maksimal 10 detik!')
+					await conn.sendMsg(m.chat, { react: { text: '⏱️', key: m.key } })
 					ch = (q.gifPlayback || q.message?.videoMessage?.gifPlayback) ? 1 : 2
 				} else ch = 0
-				if (ch > 0) buffer = await (/webp/g.test(mime) ? addExif(img, packname, author) : ch > 1 ? addExif(await video2webp(img, isNaN(fps) ? 15 : fps), packname, author) : sticker(img, false, packname, author))
+				if (ch > 0) buffer = await (/webp/g.test(mime) ? addExif(img, packname, author) : ch > 1 ? addExif(await video2webp(img, isNaN(fps) ? 10 : fps, sec), packname, author) : sticker(img, false, packname, author))
 				else {
 					try { buffer = await addExif(await sticker3(img, false), packname, author) }
 					catch { buffer = await sticker(img, false, packname, author) }
 				}
 				await conn.sendFile(m.chat, buffer, '', '', m)
-			} else return m.reply(`Kirim Gambar/Video Dengan Caption *${usedPrefix + c}*\nDurasi Video 1-9 Detik`)
+			} else return m.reply(`send img / video(0-9sec), caption *${usedPrefix + c}*`)
 		}
 	} catch (e) {
 		console.log(e)
